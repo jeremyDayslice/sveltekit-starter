8036f54b759869d5044531f737403864656e0741
Jeremy Zaborowski <jeremy@atomic.work>
2021-11-11 22:12:04 -0500

    Initial SvelteKit with a couple basic routes

---
+++ src/lib/HeaderMenu.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let name = "Timmy";
+
+    let menuItems = [
+        {
+            label: 'Home',
+            active: true,
+         },
+         {
+            label: 'About',
+            active: false,
+         },
+         {
+            label: 'FAQ',
+            active: false,
+         }
+    ]
+
+    const setActive = (index) => {
+        debugger
+        menuItems[index].active = true;
+    }
+</script>
+
+<h3>{name}</h3>
+<ul>
+    {#each menuItems as menuItem, index}
+        <li class:active={menuItem.active} on:click={( () => setActive(index))} >{menuItem}</li>
+    {/each}
+</ul>
+
+<style>
+    .active {
+        font-weight: bold; color: #47aa88;
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import HeaderMenu from "$lib/HeaderMenu.svelte";
+
+    let favNumber = 4+8
+</script>
+
+<HeaderMenu name={favNumber}></HeaderMenu>
+
+<slot></slot>
